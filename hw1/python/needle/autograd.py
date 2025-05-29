f0e672b6c6b11dbe7bc051cf30ccf6378ee53b69
Xue Lu <1571329002@qq.com>
2025-05-29 15:49:58 +0800

    finish topsort

--- hw1/python/needle/autograd.py
+++ hw1/python/needle/autograd.py
@@ -393,14 +393,24 @@ def find_topo_sort(node_list: List[Value]) -> List[Value]:
     sort.
     """
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    visited = set()
+    topo_order = []
+    for node in node_list:
+        if node not in visited:
+            topo_sort_dfs(node, visited, topo_order)
+    return topo_order
     ### END YOUR SOLUTION
 
 
 def topo_sort_dfs(node, visited, topo_order):
     """Post-order DFS"""
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    if node in visited:
+        return
+    for n in node.inputs:
+        topo_sort_dfs(n, visited, topo_order)
+    visited.add(node)
+    topo_order.append(node)
     ### END YOUR SOLUTION
 
 
