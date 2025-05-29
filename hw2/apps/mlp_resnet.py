368c012079a539c35b7e62d2dfb09ebe2a3f7811
Xue Lu <1571329002@qq.com>
2025-05-29 16:31:06 +0800

    finish resnet

--- hw2/apps/mlp_resnet.py
+++ hw2/apps/mlp_resnet.py
@@ -13,7 +13,18 @@ np.random.seed(0)
 
 def ResidualBlock(dim, hidden_dim, norm=nn.BatchNorm1d, drop_prob=0.1):
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    modules = nn.Sequential(
+        nn.Linear(dim, hidden_dim),
+        norm(hidden_dim),
+        nn.ReLU(),
+        nn.Dropout(drop_prob),
+        nn.Linear(hidden_dim, dim),   
+        norm(dim)
+    )
+    return nn.Sequential(
+        nn.Residual(modules),
+        nn.ReLU()
+    )
     ### END YOUR SOLUTION
 
 
@@ -26,14 +37,43 @@ def MLPResNet(
     drop_prob=0.1,
 ):
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    modules = [nn.Linear(dim, hidden_dim), nn.ReLU()]
+    for i in range(num_blocks):
+        modules.append(ResidualBlock(hidden_dim, hidden_dim//2, norm, drop_prob))
+    modules.append(nn.Linear(hidden_dim, num_classes))
+    return nn.Sequential(*modules)
     ### END YOUR SOLUTION
 
 
 def epoch(dataloader, model, opt=None):
     np.random.seed(4)
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    hit, total = 0, 0
+    loss_func = nn.SoftmaxLoss()
+    loss_all = 0
+    if opt is not None:
+        model.train()
+        for idx, data in enumerate(dataloader):
+            x, y = data
+            output = model(x)
+            opt.reset_grad()
+            loss = loss_func(output, y)
+            loss_all += loss.numpy()
+            loss.backward()
+            opt.step()
+            hit += (y.numpy() == output.numpy().argmax(1)).sum()
+            total += y.shape[0]
+    else:
+        model.eval()
+        for idx, data in enumerate(dataloader):
+            x, y = data
+            output = model(x)
+            loss = loss_func(output, y)
+            loss_all += loss.numpy()
+            hit += (y.numpy() == output.numpy().argmax(1)).sum()
+            total += y.shape[0]
+    err = (total - hit) / total
+    return err, loss_all / (idx + 1)
     ### END YOUR SOLUTION
 
 
@@ -48,7 +88,26 @@ def train_mnist(
 ):
     np.random.seed(4)
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    train_data = ndl.data.MNISTDataset(
+        data_dir + '/train-images-idx3-ubyte.gz',
+        data_dir + '/train-labels-idx1-ubyte.gz'
+    )
+    test_data = ndl.data.MNISTDataset(
+        data_dir + '/t10k-images-idx3-ubyte.gz',
+        data_dir + '/t10k-labels-idx1-ubyte.gz',
+    )
+    train_loader = ndl.data.DataLoader(train_data, batch_size, shuffle=True)
+    test_loader = ndl.data.DataLoader(test_data, batch_size)
+    model = MLPResNet(784, hidden_dim=hidden_dim)
+    opt = optimizer(model.parameters(), lr=lr, weight_decay=weight_decay)
+
+    print("| Epoch | Train Loss | Train Err | Test Loss | Test Err |")
+    for _ in range(epochs):
+        train_err, train_loss = epoch(train_loader, model, opt)
+        test_err, test_loss = epoch(test_loader, model)
+        print("|  {:>4} |    {:.5f} |   {:.5f} |   {:.5f} |  {:.5f} |"\
+              .format(_, train_loss, train_err, test_loss, test_err))
+    return (train_err, train_loss, test_err, test_loss)
     ### END YOUR SOLUTION
 
 
